84a4c83c406123e0bbb53cd00fe1f21ad2dc1dda
svegarab <santiago.vegara.contractor@bbva.com>
2024-06-28 14:30:44 +0200

    primeros cambios scroll

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FotoPokemonComponent } from './../../components/foto-pokemon/foto-pokemon.component';
 import { TarjetaPokemonComponent } from '../../components/tarjeta-pokemon/tarjeta-pokemon.component';
 import { PokemonService } from '../../services/pokemon.service';
@@ -14,19 +14,35 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private pokemonService: PokemonService) {
-
-  }
+  constructor(private pokemonService: PokemonService) {}
+  @ViewChild('tarjetas') tarjetasElement!:ElementRef;
 
   listaPokemon:Resultado[] = [];
+
+  pagina: number = 1;
+  cargando:boolean = false;
 
   ngOnInit(): void {
     this.cargarLista();
   }
 
   async cargarLista() {
-    this.listaPokemon = [...this.listaPokemon, ...await this.pokemonService.getByPage()];
+    this.cargando = true;
+    this.listaPokemon = [...this.listaPokemon, ...await this.pokemonService.getByPage(this.pagina)];
     console.log(this.listaPokemon);
+    this.cargando = false;
+    this.pagina ++;
+  }
+
+  onScroll(e:any){
+    if(this.cargando) return;
+    if(
+      Math.round(
+        this.tarjetasElement.nativeElement.clientHeight + this.tarjetasElement.nativeElement.scrollTop
+      )
+      === e.srcElement.scrollHeight){
+        this.cargarLista();
+      }
   }
 
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -8,15 +8,12 @@ export class PokemonService {
 
   constructor() { }
 
-  async getByPage():Promise<Resultado[]>{
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=10&offset=100");
+  async getByPage(page: number, size: number = 40):Promise<Resultado[]>{
+    const offset = size*(page-1);
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${size}&offset=${offset}`);
     const resJson = await res.json();
-    if(resJson.results.length > 0) {
-      return resJson.results;
-    }
-     
+    if(resJson.results.length > 0) return resJson.results;
     return [];
-
   }
 
   async getById(id:string) {
